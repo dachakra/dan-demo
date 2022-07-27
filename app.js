ce1686a637c5b3c8c5a8133659100beeda422899
dachakra <danny.abou-chakra@sentry.io>
2022-07-27 15:28:29 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+Sentry.init({
+  dsn: "https://db75257f1e514c0b9b61beb67ddf9be7@o1157406.ingest.sentry.io/6584955",
+});
+
+function a() {
+  b();
+}
+function b() {   
+  c();
+}
+function c() {    
+  d();
+}
+function d() {   
+  fooo();
+}
